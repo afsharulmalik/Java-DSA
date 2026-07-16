13547df1b052245828da64fec99bb2475d9bd02d
Afsharul Malik <afsharulmalik@gmail.com>
2026-07-16 23:19:43 +0530

    _22_MergeSort.java

--- src/_11_Recursion/_22_MergeSort.java
+++ src/_11_Recursion/_22_MergeSort.java
@@ -38,3 +38,60 @@ public class _22_MergeSort {
         while (j<b.length) arr[k++] = b[j++];
     }
 }
+
+
+
+
+// leetcode style
+
+class Solution {
+
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int low, int high) {
+
+        if (low >= high)
+            return;
+
+        int mid = low + (high - low) / 2;
+
+        mergeSort(nums, low, mid);
+
+        mergeSort(nums, mid + 1, high);
+
+        merge(nums, low, mid, high);
+    }
+
+    private void merge(int[] nums, int low, int mid, int high) {
+
+        int[] temp = new int[high - low + 1];
+
+        int left = low;
+        int right = mid + 1;
+        int index = 0;
+
+        while (left <= mid && right <= high) {
+
+            if (nums[left] <= nums[right]) {
+                temp[index++] = nums[left++];
+            } else {
+                temp[index++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[index++] = nums[left++];
+        }
+
+        while (right <= high) {
+            temp[index++] = nums[right++];
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+    }
+}
