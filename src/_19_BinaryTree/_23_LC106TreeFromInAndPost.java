2ee335d8ba03acd5a9bbe05929978ad75df8b38f
Afsharul Malik <afsharulmalik@gmail.com>
2026-08-18 14:34:34 +0530

    Leetcode106

---
+++ src/_19_BinaryTree/_23_LC106TreeFromInAndPost.java
@@ -0,0 +1,42 @@
+package _19_BinaryTree;
+
+public class _23_LC106TreeFromInAndPost {
+    //      Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        // helper
+        public TreeNode build(int inLow, int inHigh, int postLow, int postHigh, int[] inorder, int[] postorder){
+            if(inLow > inHigh || postLow > postHigh) return null;
+            int val = postorder[postHigh];
+            TreeNode root = new TreeNode(val);
+            int idx = 0;
+            for(int i= inLow; i<=inHigh; i++){
+                if(inorder[i] == val){
+                    idx = i;
+                    break;
+                }
+            }
+            int count = idx - inLow;
+            root.left = build(inLow, idx-1, postLow, postLow+count-1, inorder, postorder);
+            root.right = build(idx+1, inHigh, postLow+count, postHigh-1, inorder, postorder);
+            return root;
+        }
+        // main function
+        public TreeNode buildTree(int[] inorder, int[] postorder) {
+            int n = inorder.length;
+            return build(0, n-1, 0, n-1, inorder, postorder);
+        }
+    }
+}
