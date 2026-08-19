efab5399a0b793402c6cef2660e242d239e27bd6
Afsharul Malik <afsharulmalik@gmail.com>
2026-08-20 01:32:34 +0530

    leetcode230

---
+++ src/_20_BinarySearchTree/_05_LC230kthSmallestElementInBST.java
@@ -0,0 +1,33 @@
+package _20_BinarySearchTree;
+import java.util.*;
+public class _05_LC230kthSmallestElementInBST {
+
+    //      Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        // helper function
+        public void inorder(TreeNode root, ArrayList<Integer> arr){
+            if(root == null) return;
+            inorder(root.left, arr);
+            arr.add(root.val);
+            inorder(root.right, arr);
+        }
+        public int kthSmallest(TreeNode root, int k) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            inorder(root, arr);
+            return arr.get(k-1);
+        }
+    }
+}
